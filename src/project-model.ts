f7d2766069b43749731f7fefbb98fbf021d819b0
Shubham <shub_patel2295@yahoo.com>
2022-07-22 21:03:46 -0600

    Adds project and d&d namespaces

---
+++ src/project-model.ts
@@ -0,0 +1,18 @@
+namespace App {
+  //Project type
+  export enum ProjectStatus {
+    Active,
+    Finished,
+  }
+
+  // Project type class
+  export class Project {
+    constructor(
+      public id: string,
+      public title: string,
+      public description: string,
+      public people: number,
+      public status: ProjectStatus,
+    ) {}
+  }
+}
