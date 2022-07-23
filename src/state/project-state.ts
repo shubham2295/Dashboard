ac093ff8d0caa33cf25bf24cf2b564be5c35c297
Shubham <shub_patel2295@yahoo.com>
2022-07-22 21:21:12 -0600

    Adds folder structure

---
+++ src/state/project-state.ts
@@ -0,0 +1,58 @@
+namespace App {
+  //Project state management
+  type Listener<T> = (items: T[]) => void;
+
+  class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListner(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  export class ProjectState extends State<Project> {
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+      super();
+    }
+
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) {
+      const newProject = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        numOfPeople,
+        ProjectStatus.Active,
+      );
+      this.projects.push(newProject);
+      this.updateListners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+      const prj = this.projects.find((project) => project.id === projectId);
+      if (prj && prj.status !== newStatus) {
+        prj.status = newStatus;
+        this.updateListners();
+      }
+    }
+
+    updateListners() {
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+  }
+
+  // Creates singletron object
+  export const projectState = ProjectState.getInstance();
+}
